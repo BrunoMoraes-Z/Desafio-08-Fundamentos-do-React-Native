4792373c6134b4e5c98dfdefaf2cf88885add9f5
Bruno Moraes <moraes.7bruno@gmail.com>
2020-07-16 23:33:34 -0300

    finalizado desafio.

--- src/hooks/cart.tsx
+++ src/hooks/cart.tsx
@@ -30,26 +30,51 @@ const CartProvider: React.FC = ({ children }) => {
 
   useEffect(() => {
     async function loadProducts(): Promise<void> {
-      // TODO LOAD ITEMS FROM ASYNC STORAGE
+      const result = await AsyncStorage.getItem('@Marketplace:products');
+      if (result) {
+        setProducts([...JSON.parse(result)]);
+      }
     }
 
     loadProducts();
   }, []);
 
-  const addToCart = useCallback(async product => {
-    // TODO ADD A NEW ITEM TO THE CART
-  }, []);
+  const addToCart = useCallback(
+    async (product) => {
+      const exist = products.find((p) => p.id === product.id);
+      if (exist) {
+        setProducts(
+          products.map((p) => (p.id === product.id ? { ...product, quantity: p.quantity + 1 } : p)),
+        );
+      } else {
+        setProducts([...products, { ...product, quantity: 1 }]);
+      }
 
-  const increment = useCallback(async id => {
-    // TODO INCREMENTS A PRODUCT QUANTITY IN THE CART
-  }, []);
+      await AsyncStorage.setItem('@Marketplace:products', JSON.stringify(products));
+    },
+    [products],
+  );
 
-  const decrement = useCallback(async id => {
-    // TODO DECREMENTS A PRODUCT QUANTITY IN THE CART
-  }, []);
+  const increment = useCallback(async (id) => {
+    const newProducts = products.map((p) => (p.id === id ? { ...p, quantity: p.quantity + 1 } : p));
+
+    setProducts(newProducts);
+    await AsyncStorage.setItem('@Marketplace:products', JSON.stringify(newProducts));
+  }, [products]);
+
+  const decrement = useCallback(async (id) => {
+    const updatedQuantity = products.map((p) => (p.id === id ? { ...p, quantity: p.quantity - 1 } : p));
+
+    const newProducts = updatedQuantity.filter((p) => p.quantity > 0);
+
+    setProducts(newProducts);
+    await AsyncStorage.setItem('@Marketplace:products', JSON.stringify(newProducts));
+  }, [products]);
 
   const value = React.useMemo(
-    () => ({ addToCart, increment, decrement, products }),
+    () => ({
+      addToCart, increment, decrement, products,
+    }),
     [products, addToCart, increment, decrement],
   );
 
@@ -60,7 +85,7 @@ function useCart(): CartContext {
   const context = useContext(CartContext);
 
   if (!context) {
-    throw new Error(`useCart must be used within a CartProvider`);
+    throw new Error('useCart must be used within a CartProvider');
   }
 
   return context;
